496f4f906e82bd955806f0aea664fbaf97a51051
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-02-10 07:58:39 +0000

    run app in uwsgi server WIP

--- infra/terraform/templates/price-app.sh
+++ infra/terraform/templates/price-app.sh
@@ -9,16 +9,18 @@ REPOSITORY_URL='https://github.com/manrodri/price-of-chair-deployment.git'
 sudo  mkdir -p ${WORK_DIR}; sudo chown ubuntu:ubuntu ${WORK_DIR}
 sudo mkdir ${LOG_DIR}; sudo chown ubuntu:ubuntu ${LOG_DIR}
 
-sudo apt-get update; sudo apt install -y python3 pipenv nginx git
 
-cd ${WORK_DIR}; git clone ${REPOSITORY_URL} && cd price-of-chair-deployment
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt instal -y build-essential python3.7-dev python3-pip python3.7
+
+sudo apt-get update; sudo apt install -y pipenv nginx uwsgi
+
+cd ${WORK_DIR}; git clone ${REPOSITORY_URL} .
 
 # install dependencies and create venv
-
-pipenv --python $(which python3)
-pipenv shell
-pipenv install
-python ./app.py
+pipenv install --python=python3.7
+pipenv run python app.py
 
 
 
