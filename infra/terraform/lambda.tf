ea240af3921b5db6ee1f5c965e9b0c4ad4636333
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-02-22 09:33:20 +0000

    alert_updater.py running as a lambda

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -43,7 +43,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "templates/lambda/lambda_function_payload.zip"
+  filename      = "templates/lambda/deployment_package.zip"
   function_name = "alert_updater"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "alert_updater.lambda_handler"
