cb1c8110b4e8e7e02f707623833e5efb71a6c8a7
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-03-02 08:21:19 +0000

    cicd WIP

--- infra/cicd-terraform/codebuild.tf
+++ infra/cicd-terraform/codebuild.tf
@@ -108,3 +108,19 @@ resource "aws_codebuild_project" "example" {
   source_version = var.repository_branch
 
 }
+
+resource "aws_codebuild_webhook" "example" {
+  project_name = aws_codebuild_project.example.name
+  build_type   = "BUILD"
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+
+    filter {
+      type    = "HEAD_REF"
+      pattern = "master"
+    }
+  }
+}
